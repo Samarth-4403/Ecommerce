0c1ce75d383fd37c402d02f486158dabd05e4ef7
Samarth Parija <84064994+Samarth-4403@users.noreply.github.com>
2023-03-08 18:00:38 +0530

    Add files via upload

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
+import Slider from '../../components/Slider/Slider'
+import "./Home.scss"
+const Home = () => {
+    return (
+        <div className='home'>
+          <Slider/>  
+          <FeaturedProducts/>
+        </div>
+    )
+}
+
+export default Home
