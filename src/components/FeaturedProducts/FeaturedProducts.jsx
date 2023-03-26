3aad3c3d20e94f783f8824c0225e6fde1619e007
Freeze-ssp <92584308+Freeze-ssp@users.noreply.github.com>
2023-03-26 14:50:21 +0530

    Update FeaturedProducts.jsx

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,64 +1,34 @@
-import React from 'react';
+import React from "react";
 import Card from "../Card/Card";
 import "./FeaturedProducts.scss";
+import useFetch from "../../hooks/useFetch";
 
-const FeaturedProducts = ({type}) => {
+const FeaturedProducts = ({ type }) => {
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
 
-  const data = [
-    {
-      id: 1,
-      img: "/bedsheets/one.jpeg",
-      img2: "",
-      title: "Blue Handblock Cotton Bedsheet",
-      isNew: true,
-      oldPrice: 4500,
-      price: 3000,
-    },
-    {
-      id: 2,
-      img: "/bedsheets/two.jpeg",
-      title: "Grey Handblock Cotton Bedsheet",
-      isNew: true,
-      oldPrice: 5000,
-      price: 4000,
-    },
-    {
-      id: 3,
-      img: "/bedsheets/three.jpeg",
-      title: "Light Blue Handblock Cotton Bedsheet",
-      isNew: true,
-      oldPrice: 4500,
-      price: 3500,
-    },
-    {
-      id: 4,
-      img: "/bedsheets/seven.jpeg",
-      title: "Pink Handblock Cotton Bedsheet",
-      isNew: true,
-      oldPrice: 5000,
-      price: 4500,
-    },
-  ];
-
-    return (
-      <div className='featuredProducts'>FeaturedProducts 
-
-        <div className="top">
-          <h1>{type} products</h1>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Dolores id dolorem eaque tempore soluta nemo esse cupiditate 
-            laudantium obcaecati beatae blanditiis dolorum perferendis 
-            perspiciatis in dolor, rem corrupti voluptatibus earum!
-          </p>
-        </div>
-        <div className="bottom">
-          {data.map(item=>(
-            <Card item={item} key={item.id}/>
-          ))}
-        </div>
-      </div> 
-      );
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
+          lacus vel facilisis labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas.
+        </p>
+      </div>
+      <div className="bottom">
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
+      </div>
+    </div>
+  );
 };
 
-export default FeaturedProducts
+export default FeaturedProducts;
