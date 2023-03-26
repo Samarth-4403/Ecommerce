d0a876260ad7a29eb54ecd97112ce9bb1c338ad3
Freeze-ssp <92584308+Freeze-ssp@users.noreply.github.com>
2023-03-26 14:52:00 +0530

    Update Product.jsx

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,71 +1,115 @@
-import React, { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import "./Product.scss";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
-import Balance from '@mui/icons-material/Balance';
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/cartReducer";
 
 const Product = () => {
-    const [selectedImg, setSelectedImg] = useState(0)
-    const [quantity, setQuantity] = useState(1);
+  const id = useParams().id;
+  const [selectedImg, setSelectedImg] = useState("img");
+  const [quantity, setQuantity] = useState(1);
 
-    const images = [
-    "/bedsheets/four.jpeg",
-    "/bedsheets/nine.jpeg"
-    ];
+  const dispatch = useDispatch();
+  const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
 
-    return (
-        <div className='product'>
-            <div className="left">
-                <div className="images">
-                    <img src={images[0]} alt="" onClick={e=>setSelectedImg(0)} />
-                    <img src={images[1]} alt="" onClick={e=>setSelectedImg(1)} />
-                </div>
-                <div className="mainImg">
-                    <img src={images[selectedImg]} alt="" />
-                </div>
+  return (
+    <div className="product">
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          <div className="left">
+            <div className="images">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img")}
+              />
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img2?.data?.attributes?.url
+                }
+                alt=""
+                onClick={(e) => setSelectedImg("img2")}
+              />
             </div>
-            <div className="right">
-                <h1>Title</h1>
-                <span className='price'>₹5000</span>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Animi modi vitae aut, veritatis nobis, odit, ut distinctio 
-                    exercitationem voluptate aliquam assumenda necessitatibus? 
-                    Ab provident voluptas officiis laboriosam quasi repudiandae quod.
-                </p>
-                <div className="quantity">
-                    <button onClick={()=>setQuantity(prev=>prev ===1 ? 1 : prev-1)}>-</button>
-                    {quantity}
-                    <button onClick={()=>setQuantity(prev=>prev+1)}>+</button>
-                </div>
-                <button className="add">
-                    <AddShoppingCartIcon/> ADD TO CART
-                </button>
-                <div className="links">
-                    <div className="item">
-                        <FavoriteBorderIcon/> ADD TO WISH LIST
-                    </div>
-                    <div className="item">
-                        <BalanceIcon/> ADD TO COMPARE
-                    </div>
-                </div>
-                <div className="info">
-                    <span>Vendor: MassSales</span>
-                    <span>Product Type: Bedsheet</span>
-                    <span>Tag: Bedsheet, Cotton, Bed</span>
-                </div>
-                <hr />
-                <div className="info">
-                    <span>DESCRIPTION</span>
-                    <hr />
-                    <span>ADDITIONAL INFORMATION</span>
-                    <hr />
-                    <span>FAQ</span>
-                </div>
+            <div className="mainImg">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes[selectedImg]?.data?.attributes?.url
+                }
+                alt=""
+              />
             </div>
-        </div>
-    )
-}
+          </div>
+          <div className="right">
+            <h1>{data?.attributes?.title}</h1>
+            <span className="price">₹{data?.attributes?.price}</span>
+            <p>{data?.attributes?.desc}</p>
+            <div className="quantity">
+              <button
+                onClick={() =>
+                  setQuantity((prev) => (prev === 1 ? 1 : prev - 1))
+                }
+              >
+                -
+              </button>
+              {quantity}
+              <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+            </div>
+            <button
+              className="add"
+              onClick={() =>
+                dispatch(
+                  addToCart({
+                    id: data.id,
+                    title: data.attributes.title,
+                    desc: data.attributes.desc,
+                    price: data.attributes.price,
+                    img: data.attributes.img.data.attributes.url,
+                    quantity,
+                  })
+                )
+              }
+            >
+              <AddShoppingCartIcon /> ADD TO CART
+            </button>
+            <div className="links">
+              <div className="item">
+                <FavoriteBorderIcon /> ADD TO WISH LIST
+              </div>
+              <div className="item">
+                <BalanceIcon /> ADD TO COMPARE
+              </div>
+            </div>
+            <div className="info">
+              <span>Vendor: Mass Sales</span>
+              <span>Product Type: Bedsheet</span>
+              <span>Tag: Bedsheet, Cotton, Designer</span>
+            </div>
+            <hr />
+            <div className="info">
+              <span>DESCRIPTION</span>
+              <hr />
+              <span>ADDITIONAL INFORMATION</span>
+              <hr />
+              <span>FAQ</span>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default Product
+export default Product;
